b1ca1589a6f42ac9ee8262e7686a80ce7b1f2f33
lazzy07 <222lasantha@gmail.com>
2020-09-27 02:40:04 +0530

    Outliner work started

---
+++ src/renderer/components/outliner/OutlinerController.tsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faRecycle, faSync, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+export default class OutlinerController extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          paddingTop: 2,
+          paddingLeft: 10,
+          marginBottom: -8,
+          fontSize: 12,
+          display: "flex",
+        }}
+      >
+        <div className="clickable" style={{ padding: 5, marginRight: 5 }}>
+          <FontAwesomeIcon icon={faPlus} />
+        </div>
+        <div className="clickable" style={{ padding: 5, marginRight: 20 }}>
+          <FontAwesomeIcon icon={faSync} />
+        </div>
+      </div>
+    )
+  }
+}
--- src/renderer/editor_screens/OutlinerComponent.tsx
+++ src/renderer/editor_screens/OutlinerComponent.tsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import OutlinerController from '../components/outliner/OutlinerController'
+import { defaultColors } from '../constants/Colors'
 
 interface Props {
   dimensions: { width: number, height: number }
@@ -12,7 +14,16 @@ export default class OutlinerComponent extends Component<Props, State> {
   render() {
     return (
       <div>
+        <OutlinerController />
+        <div style={{
+          marginBottom: 20,
+          margin: 10,
+          width: this.props.dimensions.width - 20,
+          height: this.props.dimensions.height - 40,
+          backgroundColor: defaultColors.IMPORTANT_BACKGROUND_COLOR
+        }}>
 
+        </div>
       </div>
     )
   }
