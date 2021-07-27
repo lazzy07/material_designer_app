aee9bfb8a5a36c40ae09f3ab57f08f81d0a5e443
lazzy07 <222lasantha@gmail.com>
2021-07-27 22:50:23 +0530

    Kernel Node Editor added

--- src/renderer/screens/KernelNodeEditor.tsx
+++ src/renderer/screens/KernelNodeEditor.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { GoldenLayoutComponent } from "../components/editor_page/golden_layout/GoldenLayoutComponent";
 
 export default class KernelNodeEditor extends Component {
   render() {
