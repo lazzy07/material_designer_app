7f87a1d3cc7068f3ace572504559ba0a016228a1
lazzy07 <222lasantha@gmail.com>
2022-04-30 22:42:54 +0530

    Objects to functions issue fixed

--- src/graph_node_functionality/classes/node_classes/data_node_classes/DataNode.ts
+++ src/graph_node_functionality/classes/node_classes/data_node_classes/DataNode.ts
@@ -1,5 +1,7 @@
+import { v4 } from "uuid";
 import { Graphs, GRAPH_TYPES } from "../../../../interfaces/Graphs";
-import { Component } from "../../../../packages/rete-1.4.4";
+import { Component, Node } from "../../../../packages/rete-1.4.4";
+import { NodePropertyData } from "../../../interfaces/NodePropertyData";
 
 export default abstract class DataNode<T> extends Component {
   data: Graphs;
