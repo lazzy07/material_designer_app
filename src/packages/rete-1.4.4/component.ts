8f89f6c42531d83997065f6ed2941fb7b4639aa1
lazzy07 <222lasantha@gmail.com>
2022-05-21 20:51:11 +0530

    Rete component change

--- src/packages/rete-1.4.4/component.ts
+++ src/packages/rete-1.4.4/component.ts
@@ -1,30 +1,27 @@
-import { Component as ComponentWorker } from './engine/component';
-import { Node } from './node';
-import { NodeEditor } from './editor';
+import { Component as ComponentWorker } from "./engine/component";
+import { Node } from "./node";
+import { NodeEditor } from "./editor";
 
 export abstract class Component extends ComponentWorker {
+  editor: NodeEditor | null = null;
+  data: unknown = {};
 
-    editor: NodeEditor | null = null;
-    data: unknown = {};
+  constructor(name: string) {
+    super(name);
+  }
 
-    constructor(name: string) {
-        super(name);
-    }
+  abstract async builder(node: Node): Promise<void>;
 
-    abstract async builder(node: Node): Promise<void>;
+  async build(node: Node) {
+    await this.builder(node);
 
-    async build(node: Node) {
-        await this.builder(node);
+    return node;
+  }
 
-        return node;
-    }
+  async createNode(data = {}) {
+    let node = new Node(this.name);
 
-    async createNode(data = {}) {
-        const node = new Node(this.name);
-        
-        node.data = data;
-        await this.build(node);
-
-        return node;
-    }
+    node.data = data;
+    return node;
+  }
 }
