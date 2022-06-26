35e9a642d9a83845b8a8562eb3f78fdeef79c4a4
lazzy07 <222lasantha@gmail.com>
2022-06-27 00:25:03 +0530

    DataGraphNodeChanges

--- src/graph_node_functionality/classes/node_classes/data_node_classes/primitive_nodes/DataGraphNode.ts
+++ src/graph_node_functionality/classes/node_classes/data_node_classes/primitive_nodes/DataGraphNode.ts
@@ -1,18 +1,8 @@
-import { NodeData } from "./../../../../../packages/rete-1.4.4/core/data";
-import {
-  BOOLEAN_SOCKET,
-  COLORVEC3_SOCKET,
-  COLORVEC_SOCKET,
-  LUT3_SOCKET,
-  LUT_SOCKET,
-  NUMBER2_SOCKET,
-  NUMBER_SOCKET,
-} from "./../../../../ConnectionTypes";
-import { Input, Output, Node } from "../../../../../packages/rete-1.4.4";
+import { Node } from "../../../../../packages/rete-1.4.4";
 import { Graphs } from "../../../../../interfaces/Graphs";
 import DataNode from "../DataNode";
 import NameController from "../../../renderer/controls/NameController";
-import { getVarName } from "../../../../../renderer/services/GetNodesVarName";
+import { dataTypeToSocket } from "../../../../../renderer/services/DataTypeToSocket";
 
 export class DataGraphNode extends DataNode<any> {
   async builder(node: Node) {
@@ -26,133 +16,9 @@ export class DataGraphNode extends DataNode<any> {
 
     for (const nodeData of Object.values(nodes) as any) {
       const initID = nodeData.data.id;
-      if (initID == "1" || initID == "2" || initID == "6" || initID == "8") {
-        node.addInput(
-          new Input(
-            nodeData.id.toString(),
-            getVarName(nodeData.data as any),
-            NUMBER_SOCKET
-          )
-        );
-        isInput = true;
-      } else if (initID == "3" || initID == "9") {
-        node.addInput(
-          new Input(
-            nodeData.id.toString(),
-            getVarName(nodeData.data as any),
-            NUMBER2_SOCKET
-          )
-        );
-        isInput = true;
-      } else if (initID == "4") {
-        node.addInput(
-          new Input(
-            nodeData.id.toString(),
-            getVarName(nodeData.data as any),
-            BOOLEAN_SOCKET
-          )
-        );
-        isInput = true;
-      } else if (initID == "10") {
-        node.addInput(
-          new Input(
-            nodeData.id.toString(),
-            getVarName(nodeData.data as any),
-            LUT_SOCKET
-          )
-        );
-        isInput = true;
-      } else if (initID == "11") {
-        node.addInput(
-          new Input(
-            nodeData.id.toString(),
-            getVarName(nodeData.data as any),
-            LUT3_SOCKET
-          )
-        );
-        isInput = true;
-      } else if (initID == "12") {
-        node.addInput(
-          new Input(
-            nodeData.id.toString(),
-            getVarName(nodeData.data as any),
-            COLORVEC_SOCKET
-          )
-        );
-        isInput = true;
-      } else if (initID == "13") {
-        node.addInput(
-          new Input(
-            nodeData.id.toString(),
-            getVarName(nodeData.data as any),
-            COLORVEC3_SOCKET
-          )
-        );
-        isInput = true;
-      } else if (initID == "28") {
-        node.addOutput(
-          new Output(
-            nodeData.id.toString(),
-            getVarName(nodeData.data as any),
-            NUMBER_SOCKET
-          )
-        );
-        isOutput = true;
-      } else if (initID == "29") {
-        node.addOutput(
-          new Output(
-            nodeData.id.toString(),
-            getVarName(nodeData.data as any),
-            NUMBER2_SOCKET
-          )
-        );
-        isOutput = true;
-      } else if (initID == "30") {
-        node.addOutput(
-          new Output(
-            nodeData.id.toString(),
-            getVarName(nodeData.data as any),
-            COLORVEC_SOCKET
-          )
-        );
-        isOutput = true;
-      } else if (initID == "31") {
-        node.addOutput(
-          new Output(
-            nodeData.id.toString(),
-            getVarName(nodeData.data as any),
-            COLORVEC3_SOCKET
-          )
-        );
-        isOutput = true;
-      } else if (initID == "32") {
-        node.addOutput(
-          new Output(
-            nodeData.id.toString(),
-            getVarName(nodeData.data as any),
-            BOOLEAN_SOCKET
-          )
-        );
-        isOutput = true;
-      } else if (initID == "33") {
-        node.addOutput(
-          new Output(
-            nodeData.id.toString(),
-            getVarName(nodeData.data as any),
-            LUT_SOCKET
-          )
-        );
-        isOutput = true;
-      } else if (initID == "34") {
-        node.addOutput(
-          new Output(
-            nodeData.id.toString(),
-            getVarName(nodeData.data as any),
-            LUT3_SOCKET
-          )
-        );
-        isOutput = true;
-      }
+      const [input, output] = dataTypeToSocket(initID, node, nodeData);
+      isInput = input;
+      isOutput = output;
     }
 
     if (isInput && !isOutput) {
