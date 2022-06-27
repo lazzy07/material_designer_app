1ff604eb770aa51cde290301d79a8c7ec34a0146
lazzy07 <222lasantha@gmail.com>
2022-06-27 23:20:09 +0530

    Datareferencenode added

--- src/graph_node_functionality/classes/node_classes/data_node_classes/primitive_nodes/DataReferenceNode.ts
+++ src/graph_node_functionality/classes/node_classes/data_node_classes/primitive_nodes/DataReferenceNode.ts
@@ -1,11 +1,41 @@
+import { DataGraphDraggableElement } from "../../../../../interfaces/DataGraphDraggableElement";
+import { Graphs } from "../../../../../interfaces/Graphs";
 import { Node } from "../../../../../packages/rete-1.4.4";
+import { dataTypeToSocket } from "../../../../../renderer/services/DataTypeToSocket";
 import NameController from "../../../renderer/controls/NameController";
 import DataProcessNode from "../DataProcessNode";
 
 export class DataReferenceNode extends DataProcessNode<any> {
-  async builder(node: Node): Promise<void> {
-    node.addControl(
-      new NameController("Ref", this.data.id, this.data.name, node)
+  dragData: DataGraphDraggableElement;
+  constructor(ref: DataGraphDraggableElement) {
+    super(
+      {
+        type: "dataGraph",
+        children: [],
+        id: "any",
+        contentType: "graph",
+        name: "Dummy Data",
+      },
+      "dataGraph"
     );
+    this.dragData = ref;
+  }
+
+  async builder(node: Node): Promise<void> {
+    node.meta = { engineType: "dataGraph" };
+
+    node.addControl(
+      new NameController(
+        "Ref",
+        this.dragData.nodeId + "_" + this.dragData.dataitemId,
+        this.dragData.dataItemName,
+        node
+      )
+    );
+
+    const nodeData = node.data;
+    const initID: string = node.data.id as string;
+
+    dataTypeToSocket(initID, node, nodeData);
   }
 }
