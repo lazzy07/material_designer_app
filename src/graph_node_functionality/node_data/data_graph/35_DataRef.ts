3beda463670ef4f31720dc829e61d751f08cd9ed
lazzy07 <222lasantha@gmail.com>
2022-07-08 12:22:39 +0530

    DataRef init data added

---
+++ src/graph_node_functionality/node_data/data_graph/35_DataRef.ts
@@ -0,0 +1,16 @@
+import { Graphs } from "../../../interfaces/Graphs";
+
+export const DataRef = (): Graphs => ({
+  id: "35",
+  name: "Ref",
+  type: "dataGraph",
+  contentType: "graph",
+  children: [],
+  dataGraph: {
+    id: "351",
+    data: {},
+    parentId: "35",
+    ioType: "output",
+    operationType: "",
+  },
+});
